56e7e9cb7f518d537d3a79a04162d600d037be6e
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-10-10 17:47:42 +0700

    add taskboard page to show tasks

--- manager/urls.py
+++ manager/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("taskboard/<int:taskboard_id>/delete/", views.delete_taskboard, name="delete_taskboard"),
     path("calendar/", views.CalendarView.as_view(), name="calendar"),
     path("taskboard/<int:pk>/", views.TaskboardView.as_view(), name="taskboard"),
+    path("taskboard/<int:taskboard_id>/add_task/", views.create_task, name="create_task"),
+    path("task/<int:task_id>/delete/", views.delete_task, name="delete_task")
 ]
