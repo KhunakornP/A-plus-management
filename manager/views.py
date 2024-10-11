74c7f65e671c0b1952502fcd6a7c85292d486394
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-10-11 22:19:13 +0700

    add create_event function

--- manager/views.py
+++ manager/views.py
@@ -3,6 +3,8 @@
 from django.views import generic
 from .models import Taskboard, Event
 from django.forms import ModelForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class TaskboardView(generic.ListView):
@@ -26,3 +28,27 @@ class EventForm(ModelForm):
         """Metaclass for the form"""
         model = Event
         fields = ["title", "start_date", "end_date", "details"]
+
+    def __init__(self, *args, **kwargs):
+        """Modify the init method to specify which fields are optional"""
+        super().__init__(*args, **kwargs)
+        # Note while event does have defaults for dates please enter
+        # the date that the user clicked on the calendar for the default.
+        self.fields["details"].required = False
+
+
+def create_event(request) -> redirect:
+    """Create an event object from the POST request
+
+    :param request: A django HttpRequest object.
+    :return: A redirect to the Calendar page.
+    """
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, f"Event: {request.POST['title']} added!")
+            return redirect("manager:calendar")
+    # if method is not POST or form is not valid
+    messages.error(request, f"Event data is invalid.")
+    return redirect("manager:calendar")
