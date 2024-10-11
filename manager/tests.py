9612ec2a7bb660a8eafabb01c478955a1d877f3f
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-10-11 23:29:45 +0700

    add tests for event creation and deletion

--- manager/tests.py
+++ manager/tests.py
@@ -3,7 +3,8 @@
 from datetime import datetime, time, date
 from django.utils import timezone
 from django.test import TestCase
-from .models import Taskboard, Task
+from .models import Taskboard, Task, Event
+from django.urls import reverse
 
 
 def create_taskboard(name: str = "Today"):
@@ -90,3 +91,57 @@ class TaskModelTestcase(TestCase):
         task4 = create_task("Sketch the subject", "TODO", taskboard2)
         self.assertEqual([task1, task2], list(taskboard1.task_set.all()))
         self.assertEqual([task3, task4], list(taskboard2.task_set.all()))
+
+
+# to the group who's working on the taskboard could you please
+# refactor this to your test suit. While we're here shall we also do codecov?
+class EventViewTests(TestCase):
+    """Tests for the front end views for creating and deleting tasks"""
+
+    def test_create_valid_event(self):
+        """Test creating an event."""
+        url = reverse("manager:create_event")
+        data = {"title": "New festival", "start_date": datetime.now(),
+                "end_date": datetime.now()}
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse("manager:calendar"))
+        self.assertEqual(Event.objects.filter(title="New festival").count(), 1)
+        data = {"title": "Midterms", "start_date": datetime.now(),
+                "end_date": datetime.now()}
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse("manager:calendar"))
+        self.assertEqual(Event.objects.filter(title="Midterms").count(), 1)
+        self.assertEqual(Event.objects.count(), 2)
+
+    def test_create_invalid_task(self):
+        """
+        Test creating an invalid event.
+
+        If a required form field is empty, no event should be created.
+        """
+        url = reverse("manager:create_event")
+        data = {}  # ohno no field data provided!
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse("manager:calendar"))
+        self.assertEqual(Event.objects.count(), 0)
+
+    def test_delete_valid_task(self):
+        """Test deleting a valid event."""
+        event = Event.objects.create(title="Bug bounty ISP")
+        self.assertEqual(Event.objects.count(), 1)
+        url = reverse("manager:delete_event", args=(event.id, ))
+        response = self.client.post(url)
+        self.assertEqual(Event.objects.count(), 0)
+        self.assertRedirects(response, reverse("manager:calendar"))
+
+    def test_delete_invalid_task(self):
+        """
+        Test deleting an event that does not exist.
+
+        The user should be redirected back to the calendar page even if an
+        event could not be deleted.
+        """
+        # delete an event that does not exist (yet)
+        url = reverse("manager:delete_event", args=(9999, ))
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse("manager:calendar"))
