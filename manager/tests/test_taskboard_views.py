6477d94bb79b7875eb631e00d7719248198838b1
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-11 10:01:53 +0700

    add more tests to task and taskboard views

--- manager/tests/test_taskboard_views.py
+++ manager/tests/test_taskboard_views.py
@@ -51,15 +51,34 @@ class TaskboardTests(TestCase):
         self.assertEqual(Taskboard.objects.count(), 0)
         url = reverse("manager:delete_taskboard", args=(400,))
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertRedirects(response, reverse("manager:taskboard_index"))
         self.assertEqual(Taskboard.objects.count(), 0)
 
-    def test_modifying_taskboard(self):
-        """Test modifying the taskboard."""
+    def test_updating_taskboard(self):
+        """Test updating the taskboard's attributes."""
         tb = create_taskboard("Taskboard 1")
         new_name = "Event Generator 3000"
         new_data = {"name": new_name}
         url = reverse("manager:update_taskboard", args=(tb.id,))
         response = self.client.post(url, new_data)
-        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("manager:taskboard_index"))
         self.assertEqual(Taskboard.objects.filter(name=new_name).count(), 1)
+
+    def test_updating_non_existent_taskboard(self):
+        """Test updating non-existent taskboard with valid attributes."""
+        new_name = "Event Generator 3000"
+        new_data = {"name": new_name}
+        url = reverse("manager:update_taskboard", args=(3000,))
+        response = self.client.post(url, new_data)
+        self.assertRedirects(response, reverse("manager:taskboard_index"))
+        self.assertEqual(Taskboard.objects.count(), 0)
+
+    def test_updating_taskboard_with_invalid_data(self):
+        """Test updating a taskboard with invalid data."""
+        old_name = "Taskboard 1"
+        tb = create_taskboard(old_name)
+        url = reverse("manager:update_taskboard", args=(tb.id,))
+        response = self.client.post(url, {})
+        self.assertRedirects(response, reverse("manager:taskboard_index"))
+        self.assertEqual(Taskboard.objects.count(), 1)
+        self.assertEqual(Taskboard.objects.filter(name=old_name).count(), 1)
