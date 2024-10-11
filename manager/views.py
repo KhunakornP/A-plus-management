7b18c55b03a41ffe9bbcac68948813c602ac3658
Oak Soe Htet <oaksoe.h@ku.th>
2024-10-11 20:58:10 +0700

    implemented view for calendar. Seperated style from calendar.html

--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,9 @@
 """Module for all view classes for pages in the manager app."""
-
+import calendar
 from django.views import generic
-from .models import Taskboard
+from .models import Taskboard, Task
 from django.shortcuts import render
-from datetime import datetime
+from datetime import date
 
 
 class TaskboardView(generic.ListView):
@@ -13,10 +13,20 @@ class TaskboardView(generic.ListView):
     model = Taskboard
 
 
-def calendar_view(request):
+def calendar_view(request, date=date.today()):
     """Display calendar."""
-    today = datetime.today()
+    current_year = date.year
+    current_month = date.month
+    start_day, n_days = calendar.monthrange(current_year, current_month)
+
+    tasks = Task.objects.filter(end_date__year=current_year,
+                                 end_date__month=current_month)
+    task_data = [{'title': task.title, 'end_date': task.end_date} for task in tasks]
+
     return render(request, 'manager/calendar.html', {
-        'current_month': today.month,
-        'current_year': today.year,
+        'current_month': current_month,
+        'current_year': current_year,
+        'start_day': start_day,
+        'n_days': n_days,
+        'tasks': task_data,
     })
