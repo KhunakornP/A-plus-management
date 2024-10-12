dc1cd904b66166d9aaa600f45101d4e4df743d1e
Oak Soe Htet <oaksoe.h@ku.th>
2024-10-12 20:12:33 +0700

    reformat code with ruff

--- manager/views.py
+++ manager/views.py
@@ -1,4 +1,5 @@
 """Module for all view classes for pages in the manager app."""
+
 import calendar
 from django.views import generic
 from .models import Taskboard, Task, Event
@@ -12,22 +13,24 @@ class TaskboardView(generic.ListView):
     template_name = "manager/taskboard.html"
     model = Taskboard
 
+
 def generate_calendar(start_day, n_days):
     """Return a nested list of days representing a calendar."""
-    days = [''] * start_day + list(range(1, n_days + 1))
-    rows = [days[i:i + 7] for i in range(0, len(days), 7)]
-    
+    days = [""] * start_day + list(range(1, n_days + 1))
+    rows = [days[i : i + 7] for i in range(0, len(days), 7)]
+
     # Ensure the last row has exactly 7 elements
     if len(rows[-1]) < 7:
-        rows[-1].extend([''] * (7 - len(rows[-1])))
-    
+        rows[-1].extend([""] * (7 - len(rows[-1])))
+
     return rows
+
 
 def calendar_view(request):
     """Display calendar."""
-    year = request.GET.get('year')
-    month = request.GET.get('month')
-    day = request.GET.get('day')
+    year = request.GET.get("year")
+    month = request.GET.get("month")
+    day = request.GET.get("day")
 
     if year is None or month is None:
         year = date.today().year
@@ -42,29 +45,31 @@ def calendar_view(request):
     elif month > 12:
         month = 1
         year += 1
-        
+
     start_day, n_days = calendar.monthrange(year, month)
 
-    tasks = Task.objects.filter(end_date__year=year,
-                                end_date__month=month)
-    
+    tasks = Task.objects.filter(end_date__year=year, end_date__month=month)
+
     # Calculate the start and end of the month
     start_date = date(year, month, 1)
     end_date = start_date + timedelta(days=n_days - 1)
 
     # Filter events based on start_date or end_date within the month
-    events = Event.objects.filter(
-        start_date__lte=end_date,
-        end_date__gte=start_date)
+    events = Event.objects.filter(start_date__lte=end_date, end_date__gte=start_date)
 
-    return render(request, 'manager/calendar.html', {
-        'current_day': day,
-        'current_month': month,
-        'current_year': year,
-        'rows': generate_calendar(start_day+1, n_days),
-        'tasks': tasks,
-        'events': events,
-    })
+    return render(
+        request,
+        "manager/calendar.html",
+        {
+            "current_day": day,
+            "current_month": month,
+            "current_year": year,
+            "rows": generate_calendar(start_day + 1, n_days),
+            "tasks": tasks,
+            "events": events,
+        },
+    )
 
-if __name__ == 'main':
+
+if __name__ == "main":
     print(generate_calendar(2, 31))
