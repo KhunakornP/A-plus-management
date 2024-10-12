77f4da2f56b2f31234bb8be66b65c53b821af8de
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-10-12 11:16:51 +0700

    add test case for updating events

--- manager/tests.py
+++ manager/tests.py
@@ -7,7 +7,7 @@ from .models import Taskboard, Task, Event
 from django.urls import reverse
 
 
-def create_taskboard(name: str = "Today"):
+def create_taskboard(name: str = "Today") -> Taskboard:
     """
     Create a taskboard with the given name.
 
@@ -16,7 +16,25 @@ def create_taskboard(name: str = "Today"):
     return Taskboard.objects.create(name=name)
 
 
-def create_task(title: str, status: str, taskboard: Taskboard, end_date=None):
+def create_event(
+    title: str, start_date: datetime = None, end_date: datetime = None
+) -> Event:
+    """
+    Create an event with the given title, start date, and end date.
+
+    :param title: The name of the event.
+    :param start_date: The start date of the event.
+    :param end_date: The end date of the event.
+    :return: An Event object.
+    """
+    if not start_date:
+        start_date = timezone.now()
+    if not end_date:
+        end_date = timezone.now()
+    return Event.objects.create(title=title, start_date=start_date, end_date=end_date)
+
+
+def create_task(title: str, status: str, taskboard: Taskboard, end_date=None) -> Task:
     """
     Create a task with the given parameters.
 
@@ -119,7 +137,7 @@ class EventViewTests(TestCase):
         self.assertEqual(Event.objects.filter(title="Midterms").count(), 1)
         self.assertEqual(Event.objects.count(), 2)
 
-    def test_create_invalid_task(self):
+    def test_create_invalid_event(self):
         """
         Test creating an invalid event.
 
@@ -131,7 +149,7 @@ class EventViewTests(TestCase):
         self.assertRedirects(response, reverse("manager:calendar"))
         self.assertEqual(Event.objects.count(), 0)
 
-    def test_delete_valid_task(self):
+    def test_delete_valid_event(self):
         """Test deleting a valid event."""
         event = Event.objects.create(title="Bug bounty ISP")
         self.assertEqual(Event.objects.count(), 1)
@@ -140,7 +158,7 @@ class EventViewTests(TestCase):
         self.assertEqual(Event.objects.count(), 0)
         self.assertRedirects(response, reverse("manager:calendar"))
 
-    def test_delete_invalid_task(self):
+    def test_delete_invalid_event(self):
         """
         Test deleting an event that does not exist.
 
@@ -151,3 +169,33 @@ class EventViewTests(TestCase):
         url = reverse("manager:delete_event", args=(9999,))
         response = self.client.get(url)
         self.assertRedirects(response, reverse("manager:calendar"))
+
+    def test_update_valid_event(self):
+        """Test updating an event with valid information"""
+        event = Event.objects.create(title="Bug bounty ISP")
+        data = {
+            "title": "Team Bug bounty ISP 2024",
+            "start_date": datetime.now(),
+            "end_date": datetime.now(),
+        }
+        url = reverse("manager:update_event", args=(event.id,))
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse("manager:calendar"))
+        updated_event = Event.objects.get(pk=event.id)
+        self.assertEqual(
+            Event.objects.filter(title="Team Bug bounty ISP 2024").count(), 1
+        )
+        self.assertEqual(Event.objects.filter(title="Bug bounty ISP").count(), 0)
+        self.assertEqual(event, updated_event)
+
+    def test_update_invalid_event(self):
+        """Updating an event that does not exist does not create a new event"""
+        data = {
+            "title": "My first date",
+            "start_date": datetime.now(),
+            "end_date": datetime.now(),
+        }
+        url = reverse("manager:update_event", args=(9999,))
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse("manager:calendar"))
+        self.assertEqual(Event.objects.count(), 0)
