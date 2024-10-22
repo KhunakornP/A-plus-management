45864b03b0b739b91ca5ce9ecdc7123853b837a0
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-10-22 22:19:18 +0700

    add viewset for Event

--- manager/views/events.py
+++ manager/views/events.py
@@ -1,11 +1,15 @@
 """Views for handling event creation/deletion and update."""
 
 import json
+from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
 from django.shortcuts import redirect
 from manager.models import Event
 from django.contrib import messages
 from django.forms import ModelForm
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from manager.serializers import EventSerializer
 
 
 class EventForm(ModelForm):
@@ -88,3 +92,84 @@ def update_event(request, event_id: int) -> redirect:
             return redirect(reverse("manager:calendar"))
     messages.error(request, "Event data provided is invalid.")
     return redirect(reverse("manager:calendar"))
+
+
+class EventViewSet(viewsets.ViewSet):
+    """ViewSet fot handling Event-related operations."""
+
+    def list(self, request):
+        """
+        List all Event objects.
+
+        :param request: The HTTP request.
+        :return: Response with events.
+        """
+        queryset = Event.objects.all()
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        """
+        Create a new Event object.
+
+        :param request: The HTTP request with event data.
+        :return: Response with created event.
+        """
+        data = request.data
+        serializer = EventSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None, **kwargs):
+        """
+        Retrieve an Event object data bt ID.
+
+        :param request: The HTTP request.
+        """
+        try:
+            event = Event.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Event not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None, **kwargs):
+        """
+        Update an existing Event object by ID.
+
+        :param request: The HTTP request with updated data.
+        :param pk: Primary key of the Event to update.
+        :return: Response with updated event.
+        """
+        try:
+            event = Event.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Event not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = EventSerializer(event, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None, **kwargs):
+        """
+        Delete an Event by ID.
+        
+        :param request: The HTTP request.
+        :param pk: Primary key of the Event to delete
+        :return: Response indicating deletion status.
+        """
+        try:
+            event = Event.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Event not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- mysite/api_router.py
+++ mysite/api_router.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
-from manager.views import TaskboardViewSet
+from manager.views import TaskboardViewSet, EventViewSet
 
 router = DefaultRouter()
 router.register(r'taskboard', TaskboardViewSet, basename='taskboard')
+router.register(r'event', EventViewSet, basename='event')
