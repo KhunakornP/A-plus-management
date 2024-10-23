c2c6341f14781184f5d24106a799e5eee40e9cde
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-23 12:39:20 +0700

    add tests for EventViewSet and delete old unused methods

--- manager/tests/test_events.py
+++ manager/tests/test_events.py
@@ -1,18 +1,20 @@
 """Test cases for models."""
 
 from datetime import datetime
-import json
+from rest_framework import status
 from django.utils import timezone
 from django.test import TestCase
 from manager.models import Event
-from django.urls import reverse
+from typing import Any, Optional
 
 
 def create_event(
-    title: str, start_date: datetime = None, end_date: datetime = None
+    title: str,
+    start_date: Optional[datetime] = None,
+    end_date: Optional[datetime] = None,
 ) -> Event:
     """
-    Create an event with the given title, start date, and end date.
+    Create an event in Django models with the given title, start date, and end date.
 
     :param title: The name of the event.
     :param start_date: The start date of the event.
@@ -26,6 +28,34 @@ def create_event(
     return Event.objects.create(title=title, start_date=start_date, end_date=end_date)
 
 
+def create_event_json(
+    title: str,
+    start_date: Optional[datetime] = None,
+    end_date: Optional[datetime] = None,
+    id: Optional[int] = None,
+) -> dict[str, Any]:
+    """
+    Create an event as a JSON object with the given title, start date, and end date.
+
+    :param title: The name of the event.
+    :param start_date: The start date of the event.
+    :param end_date: The end date of the event.
+    :return: A JSON object.
+    """
+    if not start_date:
+        start_date = timezone.now()
+    if not end_date:
+        end_date = timezone.now()
+
+    data = {}
+    if id:
+        data["id"] = str(id)
+
+    data.update({"title": title, "start_date": start_date, "end_date": end_date})
+
+    return data
+
+
 # to the group who's working on the taskboard could you please
 # refactor this to your test suit. While we're here shall we also do codecov?
 class EventViewTests(TestCase):
@@ -33,24 +63,10 @@ class EventViewTests(TestCase):
 
     def test_create_valid_event(self):
         """Test creating an event."""
-        url = reverse("manager:create_event")
-        data = {
-            "title": "New festival",
-            "start_date": datetime.now(),
-            "end_date": datetime.now(),
-        }
-        response = self.client.post(url, data)
-        self.assertRedirects(response, reverse("manager:calendar"))
-        self.assertEqual(Event.objects.filter(title="New festival").count(), 1)
-        data = {
-            "title": "Midterms",
-            "start_date": datetime.now(),
-            "end_date": datetime.now(),
-        }
-        response = self.client.post(url, data)
-        self.assertRedirects(response, reverse("manager:calendar"))
-        self.assertEqual(Event.objects.filter(title="Midterms").count(), 1)
-        self.assertEqual(Event.objects.count(), 2)
+        event = create_event_json("Hello")
+        self.client.post("/api/events/", event, format="json")
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(Event.objects.first().title, "Hello")
 
     def test_create_invalid_event(self):
         """
@@ -58,20 +74,15 @@ class EventViewTests(TestCase):
 
         If a required form field is empty, no event should be created.
         """
-        url = reverse("manager:create_event")
-        data = {}  # ohno no field data provided!
-        response = self.client.post(url, data)
-        self.assertRedirects(response, reverse("manager:calendar"))
+        self.client.post("/api/events/", {}, format="json")
         self.assertEqual(Event.objects.count(), 0)
 
     def test_delete_valid_event(self):
         """Test deleting a valid event."""
-        event = Event.objects.create(title="Bug bounty ISP")
+        event = create_event("hello")
         self.assertEqual(Event.objects.count(), 1)
-        url = reverse("manager:delete_event", args=(event.id,))
-        response = self.client.post(url)
+        self.client.delete(f"/api/events/{event.id}/")
         self.assertEqual(Event.objects.count(), 0)
-        self.assertRedirects(response, reverse("manager:calendar"))
 
     def test_delete_invalid_event(self):
         """
@@ -80,39 +91,46 @@ class EventViewTests(TestCase):
         The user should be redirected back to the calendar page even if an
         event could not be deleted.
         """
-        # delete an event that does not exist (yet)
-        url = reverse("manager:delete_event", args=(9999,))
-        response = self.client.get(url)
-        self.assertRedirects(response, reverse("manager:calendar"))
+        event = create_event("hello")
+        self.client.delete(f"/api/events/{event.id * 420 // 69}/")
+        self.assertEqual(Event.objects.count(), 1)
 
     def test_update_valid_event(self):
         """Test updating an event with valid information."""
-        event = Event.objects.create(title="Bug bounty ISP")
-        data = {
-            "title": "Team Bug bounty ISP 2024",
-            "start_date": datetime.now().isoformat(),
-            "end_date": datetime.now().isoformat(),
-        }
-        url = reverse("manager:update_event", args=(event.id,))
-        response = self.client.post(
-            url, json.dumps(data), content_type="application/json"
+        event = create_event("goodbye")
+        new_event = create_event_json(title="yahallo", id=event.id)
+        response = self.client.put(
+            f"/api/events/{event.id}/",
+            new_event,
+            format="json",
+            content_type="application/json",
         )
-        self.assertRedirects(response, reverse("manager:calendar"))
-        updated_event = Event.objects.get(pk=event.id)
-        self.assertEqual(
-            Event.objects.filter(title="Team Bug bounty ISP 2024").count(), 1
-        )
-        self.assertEqual(Event.objects.filter(title="Bug bounty ISP").count(), 0)
-        self.assertEqual(event, updated_event)
+        event.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(event.title, "yahallo")
 
     def test_update_invalid_event(self):
         """Updating an event that does not exist does not create a new event."""
-        data = {
-            "title": "My first date",
-            "start_date": datetime.now(),
-            "end_date": datetime.now(),
-        }
-        url = reverse("manager:update_event", args=(9999,))
-        response = self.client.post(url, data)
-        self.assertRedirects(response, reverse("manager:calendar"))
-        self.assertEqual(Event.objects.count(), 0)
+        event = create_event("goodbye")
+        new_event = create_event_json("yahallo")
+        response = self.client.put(
+            f"/api/events/{event.id * 420 // 69}/",
+            new_event,
+            format="json",
+            content_type="application/json",
+        )
+        event.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(event.title, "goodbye")
+
+    def test_update_event_with_invalid_data(self):
+        """Updating an event with invalid data should raise HTTP 400."""
+        event = create_event("goodbye")
+        new_event = {"name": 1234}
+        response = self.client.put(f"/api/events/{event.id}/", new_event, format="json")
+        event.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Event.objects.count(), 1)
+        self.assertEqual(event.title, "goodbye")
--- manager/views/events.py
+++ manager/views/events.py
@@ -1,11 +1,7 @@
 """Views for handling event creation/deletion and update."""
 
-import json
 from django.core.exceptions import ObjectDoesNotExist
-from django.urls import reverse
-from django.shortcuts import redirect
 from manager.models import Event
-from django.contrib import messages
 from django.forms import ModelForm
 from rest_framework import status, viewsets
 from rest_framework.response import Response
@@ -27,71 +23,6 @@ class EventForm(ModelForm):
         # Note while event does have defaults for dates please enter
         # the date that the user clicked on the calendar for the default.
         self.fields["details"].required = False
-
-
-def create_event(request) -> redirect:
-    """
-    Create an event object from the POST request.
-
-    :param request: A django HttpRequest object.
-    :return: A redirect to the Calendar page.
-    """
-    if request.method == "POST":
-        form = EventForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.info(request, f"Event: {request.POST['title']} added!")
-            return redirect(reverse("manager:calendar"))
-    # if method is not POST or form is not valid
-    messages.error(request, "Event data is invalid.")
-    return redirect(reverse("manager:calendar"))
-
-
-def delete_event(request, event_id: int) -> redirect:
-    """
-    Delete the given event from the database.
-
-    :param request: A django HttpRequest object.
-    :param event_id: The Primary Key (id) of the event.
-    :return: A redirect to the calendar page.
-    """
-    try:
-        event = Event.objects.get(pk=event_id)
-    except Event.DoesNotExist:
-        messages.error(request, "Event does not exist")
-        return redirect(reverse("manager:calendar"))
-    event_title = event.title
-    event.delete()
-    messages.info(request, f"Event: {event_title}, has been deleted.")
-    return redirect(reverse("manager:calendar"))
-
-
-def update_event(request, event_id: int) -> redirect:
-    """
-    Update an event attributes with the given data.
-
-    This function takes a POST request and updates the event with the given
-    event_id. It updates all attributes specified in the POST request for the
-    event object.
-
-    :param request: A django HttpRequest object.
-    :param event_id: The Primary Key (id) of the event.
-    :return: A redirect to the calendar page.
-    """
-    try:
-        event = Event.objects.get(pk=event_id)
-    except Event.DoesNotExist:
-        messages.error(request, "Event does not exist")
-        return redirect(reverse("manager:calendar"))
-    if request.method == "POST":
-        data = json.loads(request.body)
-        form = EventForm(data, instance=event)
-        if form.is_valid():
-            form.save()
-            messages.info(request, f"Event: {data['title']} updated.")
-            return redirect(reverse("manager:calendar"))
-    messages.error(request, "Event data provided is invalid.")
-    return redirect(reverse("manager:calendar"))
 
 
 class EventViewSet(viewsets.ViewSet):
