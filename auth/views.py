b09e792232afc2b20171441566c54c352fd2577c
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-10-23 17:52:27 +0700

    implement authentication for user using REST api

--- auth/views.py
+++ auth/views.py
@@ -4,7 +4,10 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 from urllib.parse import urljoin, urlparse, parse_qs
-
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
 import requests
 from django.urls import reverse
 from rest_framework import status
@@ -28,13 +31,20 @@ class GoogleLoginCallback(APIView):
         if code is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        # Remember to replace the localhost:8000 with the actual domain name before deployment
         token_endpoint_url = urljoin("http://localhost:8000",
                                      reverse("google_login"))
-        response = requests.post(url=token_endpoint_url, data={"code": code})
+        params = {
+            'process' : 'login',
+        }
 
-        return Response(response, status=status.HTTP_200_OK)
-
+        token_valid = False
+        response = requests.post(url=token_endpoint_url, data={"code": code}, params=params)
+        key = response.json()["key"]
+        user_id = Token.objects.get(key=key).user_id
+        user = User.objects.get(pk=user_id)
+        login(request, user,
+              backend="allauth.account.auth_backends.AuthenticationBackend")
+        return redirect(reverse("manager:taskboard_index"))
 
 
 class GoogleLogin(SocialLoginView):
