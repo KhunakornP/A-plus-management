369848721140c4ae885035d6f22b7c6b75fd31cc
Oak Soe Htet <erikbartholemew@gmail.com>
2024-10-25 13:04:47 +0700

    tried to create api ur;

--- manager/views/burndown.py
+++ manager/views/burndown.py
@@ -10,7 +10,7 @@ from typing import Optional
 from manager.models import Taskboard
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, viewsets
 
 
 def get_taskboard(taskboard_id: int) -> Optional[Taskboard]:
@@ -31,40 +31,7 @@ def get_estimate_history_data(taskboard_id):
     return EstimateHistory.objects.filter(taskboard=taskboard).order_by("date")
 
 
-def create_figure(estimate_histories):
-    """Create figure from estimate_history data."""
-    pass
-
-
-class BurndownView(generic.View):
-    """A view for the burndown chart page."""
-
-    template_name = "manager/burndown.html"
-
-    def get_context_data(self, **kwargs):
-        """Get context data for burndown chart view."""
-        context = {}
-        taskboard_id = self.kwargs.get("taskboard_id")
-        context["taskboard_id"] = taskboard_id
-        if "events" in kwargs:
-            context["events"] = kwargs["events"]
-        estimate_histories = get_estimate_history_data(taskboard_id)
-        fig = create_figure(estimate_histories)
-        context["burndown"] = fig
-        return context
-
-    def get(self, request, *args, **kwargs):
-        """Render burndown chart page when there's a GET request."""
-        context = self.get_context_data(**kwargs)
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        """Render burndown chart page when there's a POST request."""
-        kwargs["events"] = request.POST.getlist("events")
-        context = self.get_context_data(**kwargs)
-        return render(request, self.template_name, context)
-
-class EstimateHistoryData(generics.ListAPIView):
+class EstimateHistoryViewset(viewsets.ViewSet):
     """A view getting EstimateHistory data."""
     
     serializer_class = EstimateHistorySerialzer
@@ -79,37 +46,63 @@ class EstimateHistoryData(generics.ListAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['GET', 'POST'])
-def estimate_histories_json(request, taskboard_id):
-    """Return EstimateHistory as a json file."""
-    if request.method == 'GET':
-        estimate_histories = get_estimate_history_data(taskboard_id)
-        eh_serializer = EstimateHistorySerialzer(estimate_histories, many=True)
-        return Response(eh_serializer.data)
-    
-    elif request.method == 'POST':
-        eh_serializer = EstimateHistorySerialzer(data=request.data)
-        if eh_serializer.is_valid():
-            eh_serializer.save()
-            return Response(eh_serializer.data, status=status.HTTP_201_CREATED)
+class BurndownView(generic.View):
+    """A view for the burndown chart page."""
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def estimate_histories_detail(request, taskboard_id, eh_id):
-    """Return or update data of, or delete a specific EstimateHistory as a json file."""
-    try:
-        eh = EstimateHistory.objects.get(pk=eh_id)
-    except EstimateHistory.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    template_name = "manager/burndown.html"
+
+    def get_context_data(self, **kwargs):
+        """Get context data for burndown chart view."""
+        context = {}
+        taskboard_id = self.kwargs.get("taskboard_id")
+        context["taskboard_id"] = taskboard_id
+        if "events" in kwargs:
+            context["events"] = kwargs["events"]
+        return context
+
+    def get(self, request, *args, **kwargs):
+        """Render burndown chart page when there's a GET request."""
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        """Render burndown chart page when there's a POST request."""
+        kwargs["events"] = request.POST.getlist("events")
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+
+# @api_view(['GET', 'POST'])
+# def estimate_histories_json(request, taskboard_id):
+#     """Return EstimateHistory as a json file."""
+#     if request.method == 'GET':
+#         estimate_histories = get_estimate_history_data(taskboard_id)
+#         eh_serializer = EstimateHistorySerialzer(estimate_histories, many=True)
+#         return Response(eh_serializer.data)
     
-    if request.method == 'GET':
-        eh_serializer = EstimateHistorySerialzer(eh)
-        return Response(eh_serializer.data)
-    elif request.method == 'PUT':
-        eh_serializer = EstimateHistorySerialzer(eh, data=request.data)
-        if eh_serializer.is_valid():
-            eh_serializer.save()
-            return Response(eh_serializer.data)
-        return Response(eh_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        eh.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'POST':
+#         eh_serializer = EstimateHistorySerialzer(data=request.data)
+#         if eh_serializer.is_valid():
+#             eh_serializer.save()
+#             return Response(eh_serializer.data, status=status.HTTP_201_CREATED)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def estimate_histories_detail(request, taskboard_id, eh_id):
+#     """Return or update data of, or delete a specific EstimateHistory as a json file."""
+#     try:
+#         eh = EstimateHistory.objects.get(pk=eh_id)
+#     except EstimateHistory.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         eh_serializer = EstimateHistorySerialzer(eh)
+#         return Response(eh_serializer.data)
+#     elif request.method == 'PUT':
+#         eh_serializer = EstimateHistorySerialzer(eh, data=request.data)
+#         if eh_serializer.is_valid():
+#             eh_serializer.save()
+#             return Response(eh_serializer.data)
+#         return Response(eh_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         eh.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
