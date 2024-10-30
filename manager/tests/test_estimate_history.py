04987b9a1d4f003d91442f34bcd80e7a1106aaae
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-30 14:47:00 +0700

    add tests for changing task status

--- manager/tests/test_estimate_history.py
+++ manager/tests/test_estimate_history.py
@@ -12,9 +12,9 @@ class EstimateHistoryTest(BaseTestCase):
     def test_adding_same_day_tasks(self):
         """Test adding multiple tasks within the same day."""
         tb = create_taskboard(self.user1, "test")
-        Task.objects.create(title="someting", taskboard=tb, time_estimate=3)
-        Task.objects.create(title="someting3", taskboard=tb, time_estimate=5)
-        Task.objects.create(title="someting2", taskboard=tb, time_estimate=7)
+        Task.objects.create(title="something", taskboard=tb, time_estimate=3)
+        Task.objects.create(title="something3", taskboard=tb, time_estimate=5)
+        Task.objects.create(title="something2", taskboard=tb, time_estimate=7)
         self.assertEqual(EstimateHistory.objects.count(), 1)
         eh = EstimateHistory.objects.first()
         self.assertEqual(eh.time_remaining, 3 + 5 + 7)
@@ -22,7 +22,7 @@ class EstimateHistoryTest(BaseTestCase):
     def test_changing_time_estimate(self):
         """Test changing time estimate of a task."""
         tb = create_taskboard(self.user1, "test")
-        t = Task.objects.create(title="someting2", taskboard=tb, time_estimate=7)
+        t = Task.objects.create(title="something2", taskboard=tb, time_estimate=7)
         t.time_estimate = 6
         t.save()
         eh = EstimateHistory.objects.first()
@@ -53,9 +53,49 @@ class EstimateHistoryTest(BaseTestCase):
     def test_deleting_task_also_subtract_estimate_time(self):
         """If a task got deleted, the time estimate should go down as well."""
         tb = create_taskboard(self.user1, "test")
-        t = Task.objects.create(title="someting2", taskboard=tb, time_estimate=7)
+        t = Task.objects.create(title="something2", taskboard=tb, time_estimate=7)
         eh = EstimateHistory.objects.first()
         self.assertEqual(eh.time_remaining, 7)
         t.delete()
         eh.refresh_from_db()
+        self.assertEqual(eh.time_remaining, 0)
+
+    def test_changing_status(self):
+        """Test changing between different task status.
+
+        Changing task status to DONE should subtracts the task's time remaining
+        from the time estimate. Changing it back to DONE should add it back up.
+        """
+        tb = create_taskboard(self.user1, "test")
+        t = Task.objects.create(title="something1", taskboard=tb, time_estimate=7)
+        t2 = Task.objects.create(title="something2", taskboard=tb, time_estimate=3)
+        t.save()
+        t2.save()
+        eh = EstimateHistory.objects.first()
+        self.assertEqual(eh.time_remaining, 7 + 3)
+        for _ in range(2):
+            t.status = "DONE"
+            t.save()
+            eh.refresh_from_db()
+            self.assertEqual(eh.time_remaining, 0 + 3)
+        t.status = "IN PROGRESS"
+        t.save()
+        eh.refresh_from_db()
+        self.assertEqual(eh.time_remaining, 7 + 3)
+        t.status = "TODO"
+        t.save()
+        eh.refresh_from_db()
+        self.assertEqual(eh.time_remaining, 7 + 3)
+
+    def test_creating_task_with_status_done(self):
+        """Create a task where the status is set to DONE.
+
+        It should not add the time estimate.
+        """
+        tb = create_taskboard(self.user1)
+        t2 = Task.objects.create(
+            title="something2", taskboard=tb, time_estimate=3, status="DONE"
+        )
+        t2.save()
+        eh = EstimateHistory.objects.first()
         self.assertEqual(eh.time_remaining, 0)
