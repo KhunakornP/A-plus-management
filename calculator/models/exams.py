18a7c6fe940ee849c98ae1e4e2ea3d8777b9c8f4
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-10-31 21:43:32 +0700

    add model for tests

---
+++ calculator/models/exams.py
@@ -0,0 +1,18 @@
+"""Model for storing exam data."""
+
+from django.db import models
+
+
+class Exams(models.Model):
+    """
+    A class representing A-level tests.
+
+    Each Exam has a name and max possible score.
+    The core field indicates if the Exam was created by CUPT or if it
+    is issued by an outside authority, True if created by CUPT, False otherwise.
+    """
+
+    name = models.Charfield(max_length=200)
+    max_score = models.FloatField(default=100.00)
+    core = models.BooleanField(default=False)
+
