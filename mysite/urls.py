fe515579e8b13a8a171e2e3f12ef2235f2cb8407
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-02 17:41:36 +0700

    fix calculator url

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('manager/', include('manager.urls')),
     path('api/auth/', include('auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('calculator', include('calculator.urls'))
+    path('calculator/', include('calculator.urls'))
 ]
