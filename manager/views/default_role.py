4ad787f4c713bf651136caf7439800a50e608d77
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-11-04 21:36:06 +0700

    create account setup page

--- manager/views/default_role.py
+++ manager/views/default_role.py
@@ -7,6 +7,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.shortcuts import redirect, reverse
 
 
 class LoginView(TemplateView):
@@ -24,6 +25,25 @@ class LoginView(TemplateView):
         context["callback_uri"] = settings.GOOGLE_CALLBACK_URI
         context["client_id"] = settings.GOOGLE_CLIENT_ID
         return context
+
+
+class UserSetupView(TemplateView):
+    """A view for the user to select their 'role'."""
+
+    template_name = "manager/account_creation.html"
+
+    def get(self, request, *args, **kwargs):
+        """
+        Override the GET request and check if the user has set up their
+        account. Redirect the user to the main page if they already have.
+        """
+        # check if user is logged in
+        if not self.request.user.is_authenticated:
+            return redirect(reverse("manager:main_login"))
+        # check if user has already set up their account
+        if self.request.user.has_perm("manager.is_verified"):
+            return redirect(reverse("manager:taskboard_index"))
+        return super().get(request, *args, **kwargs)
 
 
 @receiver(post_save, sender=User)
