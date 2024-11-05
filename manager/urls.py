f271003aedd6fe9407b28979c3f568f95f973557
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-11-05 21:32:01 +0700

    add parent dashboard page template

--- manager/urls.py
+++ manager/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = "manager"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="main_login"),
-    path("taskboard/", views.TaskboardIndexView.as_view(), name="taskboard_index"),
     path("setup/", views.UserSetupView.as_view(), name="user_setup"),
+    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("taskboard/", views.TaskboardIndexView.as_view(), name="taskboard_index"),
     path("taskboard/<int:pk>/", views.TaskboardView.as_view(), name="taskboard"),
     path("calendar/", views.CalendarView.as_view(), name="calendar"),
     path("taskboard/<int:taskboard_id>/burndown/", views.BurndownView.as_view(), name="burndown_chart"),
