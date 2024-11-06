3fdb9e58ed30e21cffc25de61da6e659655ebf0f
Oak Soe Htet <erikbartholemew@gmail.com>
2024-11-06 19:08:19 +0700

    ruff format

--- manager/tests/test_burndown_views.py
+++ manager/tests/test_burndown_views.py
@@ -20,25 +20,23 @@ class EstimateHistoryViewTests(TestCase):
         )
         self.user.set_password(self.password)
         self.user.save()
-        self.client.login(username=self.username, password=self.password)  
+        self.client.login(username=self.username, password=self.password)
 
         self.tb = create_taskboard(self.user, "Test Taskboard")
 
         self.today = date.today()
-        self.tomorrow =  date.today() + timedelta(days=1)
-        self.aftertomorrow =  date.today() + timedelta(days=2)
+        self.tomorrow = date.today() + timedelta(days=1)
+        self.aftertomorrow = date.today() + timedelta(days=2)
 
         self.eh1 = create_estimate_hisotry(self.tb, self.today, 70)
         self.eh2 = create_estimate_hisotry(self.tb, self.tomorrow, 60)
         self.eh3 = create_estimate_hisotry(self.tb, self.aftertomorrow, 40)
-
 
     def test_get_eh_of_taskboard(self):
         """Test getting estimate_history in a specific taskboard."""
         response = self.client.get(f"/api/estimate_history/?taskboard={self.tb.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
-
 
     def test_get_invalid_taskboard(self):
         """Test getting a non-existent estimate history info."""
