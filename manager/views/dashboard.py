6c62c506ec24e418a6f125af1612397b6d2a7577
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-11-06 23:10:19 +0700

    add statistics to dashboard

--- manager/views/dashboard.py
+++ manager/views/dashboard.py
@@ -1,7 +1,9 @@
 """Views for the parent dashboard."""
-
+from django.db.models import Q
 from django.shortcuts import redirect, reverse
+from django.utils import timezone
 from django.views import generic
+from manager.models import Task
 
 
 class DashboardView(generic.ListView):
@@ -13,6 +15,24 @@ class DashboardView(generic.ListView):
     def get_queryset(self):
         """Returns all children associated with the user."""
         return self.request.user.student_set.all()
+
+    def get_context_data(self, **kwargs):
+        """Pass more task data for all related students."""
+        students = self.request.user.student_set.all()
+        context = super().get_context_data(**kwargs)
+        if students:
+            today = []
+            late = []
+            fin = []
+            for student in students:
+                today.append(Task.objects.filter(taskboard__user=student.user, end_date__day=timezone.now().day).count())
+                late.append(Task.objects.filter(~Q(status="DONE"), taskboard__user=student.user, end_date__lte=timezone.now()).count())
+                fin.append(Task.objects.filter(taskboard__user=student.user, status="DONE").count())
+            context["today"] = today
+            context["late"] = late
+            context["fin"] = fin
+        return context
+
 
     def get(self, request, *args, **kwargs):
         """
