aad8346dcdfc1994011573be4cfe124c6de8b664
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-11-19 22:34:36 +0700

    add task status chart to chart index page

--- manager/static/manager/chart_index.js
+++ manager/static/manager/chart_index.js
@@ -1,20 +1,60 @@
+const userID = JSON.parse(document.getElementById('user_id').textContent)
+const response = await fetch(`/api/tasks/?user=${userID}`);
+const tasks = await response.json();
+const today = new Date();
+const todayString = today.toISOString();
+const toDoCount = tasks.filter((task) => task.status === 'TODO').length;
+const inProgressCount = tasks.filter((task) => task.status === 'IN PROGRESS').length;
+const doneCount = tasks.filter((task) => task.status === 'DONE').length;
+const lateCount = tasks.filter((task) => task.end_date <= todayString).length;
+
+
+
 const ctx = document.getElementById('overview-chart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+const data = [{
+    data: [toDoCount, doneCount, lateCount, inProgressCount],
+    backgroundColor: [
+        "#228B22",
+        "#5f255f",
+        "#d21243",
+        "#B27200"
+    ],
+    borderColor: "#fff"
+}];
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+        enabled: false
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    plugins: {
+        legend: {
+           onClick: null
+        },
+        datalabels: {
+            formatter: (value, ctx) => {
+                const datapoints = ctx.chart.data.datasets[0].data
+                const total = datapoints.reduce((total, datapoint) => total + datapoint, 0)
+                const percentage = value / total * 100
+                if (percentage > 0){
+                return percentage.toFixed(2) + "%";
+                } else {
+                return ''
+                }
+            },
+            color: '#fff',
         }
-      }
     }
-  });
+};
+
+const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['TODO', 'Finished', 'Late', 'In Progress'],
+        datasets: data
+    },
+    options: options,
+    plugins: [ChartDataLabels],
+});
