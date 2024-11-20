7dc5bdaf0ed5188d49eafb2386bf904b2891293d
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-11-20 14:25:25 +0700

    add recent velocity for each taskboard to chart index

--- manager/static/manager/chart_index.js
+++ manager/static/manager/chart_index.js
@@ -1,13 +1,30 @@
+const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000
 const userID = JSON.parse(document.getElementById('user_id').textContent)
 const response = await fetch(`/api/tasks/?user=${userID}`);
 const tasks = await response.json();
 const today = new Date();
+const prevWeek = new Date(today.getTime() - SEVEN_DAYS).toISOString().slice(0, 10);
 const todayString = today.toISOString();
 const toDoCount = tasks.filter((task) => task.status === 'TODO').length;
 const inProgressCount = tasks.filter((task) => task.status === 'IN PROGRESS').length;
 const doneCount = tasks.filter((task) => task.status === 'DONE').length;
-const lateCount = tasks.filter((task) => task.end_date <= todayString).length;
+const lateCount = tasks.filter((task) => task.end_date <= todayString && task.status !== 'DONE').length;
 
+async function fetchRecentVelocity(taskboardID) {
+    const response = await fetch(`/api/velocity/?taskboard=${taskboardID}&start=${prevWeek}&mode=average`);
+    const Velocity = await response.json();
+    return Velocity
+}
+
+const taskboardVelocities = document.querySelectorAll('[id^="tb-"]');
+for (let i = 0; i < taskboardVelocities.length; i++) {
+    let velocity = await fetchRecentVelocity(taskboardVelocities[i].id.split('-').slice(-1)[0])
+    if (velocity > 0){
+    taskboardVelocities[i].innerHTML = `<div>Recent velocity: ${velocity}</div>`;
+    } else {
+    taskboardVelocities[i].innerHTML = `<div>Recent velocity: 0</div>`;
+    }
+}
 
 
 const ctx = document.getElementById('overview-chart');
